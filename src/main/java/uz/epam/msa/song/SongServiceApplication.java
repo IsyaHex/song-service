ea0b75e945ea1a66c22118d8cf03cccc9edf58d4
IsyaHex <i.domlajanov@gmail.com>
2023-04-09 14:44:52 +0500

    API Gateway configuration & failing tests

--- src/main/java/uz/epam/msa/song/SongServiceApplication.java
+++ src/main/java/uz/epam/msa/song/SongServiceApplication.java
@@ -3,11 +3,13 @@ package uz.epam.msa.song;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("uz.epam.msa.song.repository")
 @EntityScan("uz.epam.msa.song.domain")
+@EnableEurekaClient
 public class SongServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SongServiceApplication.class, args);
